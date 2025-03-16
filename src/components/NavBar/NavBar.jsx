fdb2e303857e77662e3f7b86d451b6b8f98d248d
Petyaev Bair <petyaev01@mail.ru>
2025-03-16 19:24:14 +0300

    [feat]: Второе домашнее задание
    Добавлена карточка фильма и список для отображения

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,11 +4,19 @@ export default function NavBar() {
 
   return (
     <nav className='navbar-style'>
-        <img src='/note.svg'/>
+        <img className='nav-logo' src='/note.svg'/>
         <div className='item-list-style'>
+          <div className='item-container'>
             <a className='item-link-style'>Поиск фильмов</a>
+          </div>
+          <div className='item-container'>
             <a className='item-link-style'>Мои фильмы</a>
+            <img src="./my_movies.svg"/>
+          </div>
+          <div className='item-container'>
             <a className='item-link-style'>Войти</a>
+            <img src="./logout.svg"/>
+          </div>
         </div>
     </nav>
   )
--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -6,4 +6,4 @@ export default function Title({text}) {
             {text}
         </h1>
     );
-}
+};
